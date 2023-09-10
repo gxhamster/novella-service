886985f794bf7a9c21605705a2f9f703fe77b2bd
Iyaan Azeez <iyaanazeez757@gmail.com>
2023-09-10 11:33:05 +0500

    Added: SVG icons refactored to seperate components

---
+++ src/components/icons/UnreturnedBookIcon.tsx
@@ -0,0 +1,24 @@
+type UnreturnedBookIconProps = {
+  size?: number;
+};
+
+export default function UnreturnedBookIcon({
+  size = 24,
+}: UnreturnedBookIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="9 10 4 15 9 20" />
+      <path d="M20 4v7a4 4 0 0 1-4 4H4" />
+    </svg>
+  );
+}
