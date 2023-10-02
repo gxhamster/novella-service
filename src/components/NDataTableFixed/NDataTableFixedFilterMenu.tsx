7783bd52c9c1ad6244d98ec84196a652cf6c7484
Iyaan Azeez <iyaanazeez757@gmail.com>
2023-10-02 13:46:33 +0500

    Refactor: Cleaned up code in Filter and Sort Menu

--- src/components/NDataTableFixed/NDataTableFixedFilterMenu.tsx
+++ src/components/NDataTableFixed/NDataTableFixedFilterMenu.tsx
@@ -1,12 +1,137 @@
-import { useState } from "react";
-import { Popover } from "@headlessui/react";
-import ButtonGhost from "../ButtonGhost";
+import { Dispatch, SetStateAction, useMemo, useState } from "react";
 import FilterIcon from "../icons/FilterIcon";
 import AddIcon from "../icons/AddIcon";
 import CloseIcon from "../icons/CloseIcon";
-import ButtonSecondary from "../ButtonSecondary";
 import Select from "../Select";
 import { NDataTableFixedFilter } from ".";
+import NDataTableFixedMenu from "./NDataTableFixedMenu";
+import NButton from "../NButton";
+
+function DisplayEmptyFilters() {
+  return (
+    <>
+      <span className="text-surface-700">No filter applied to this table</span>
+      <span className="text-surface-600 text-xs">
+        Add a column below to filter the view
+      </span>
+    </>
+  );
+}
+
+type FilterItemProps = {
+  filter: NDataTableFixedFilter;
+  filterItemChanged: (filter: NDataTableFixedFilter) => void;
+  filterItemRemoved: () => void;
+  tableProps: any[];
+};
+function FilterItem({
+  filter,
+  filterItemChanged,
+  filterItemRemoved,
+  tableProps,
+}: FilterItemProps) {
+  return (
+    <div key={filter.id} className="w-full flex-grow flex">
+      <Select
+        value={filter.prop}
+        className="flex-grow"
+        onChange={(e) => {
+          filterItemChanged({ ...filter, prop: e.target.value });
+        }}
+      >
+        {tableProps.map((prop: any) => (
+          <option key={prop}>{prop}</option>
+        ))}
+      </Select>
+      <Select
+        value={filter.operator}
+        onChange={(e) => {
+          filterItemChanged({ ...filter, operator: e.target.value });
+        }}
+      >
+        {["eq", "gt", "lt"].map((v) => (
+          <option key={v}>{v}</option>
+        ))}
+      </Select>
+      <input
+        value={filter.value}
+        onChange={(e) => {
+          e.preventDefault();
+          filterItemChanged({ ...filter, value: e.target.value });
+        }}
+        className="py-1 px-2 block min-w-[5rem] bg-surface-200 appearance-none outline-none hover:bg-surface-300 border-[1px] border-surface-200 focus:border-surface-900 text-xs text-surface-900 placeholder:text-surface-500"
+        placeholder="Enter your value here"
+      ></input>
+      <NButton
+        kind="ghost"
+        icon={<CloseIcon size={12} />}
+        onClick={filterItemRemoved}
+      />
+    </div>
+  );
+}
+
+type FilterGroupProps = {
+  filters: NDataTableFixedFilter[];
+  setFilters: Dispatch<SetStateAction<NDataTableFixedFilter[]>>;
+  tableProps: any;
+};
+
+function FilterGroup({ filters, setFilters, tableProps }: FilterGroupProps) {
+  return (
+    <div className="flex flex-col gap-3 items-center w-full text-surface-600">
+      {filters.map((filter, idx) => (
+        <FilterItem
+          filter={filter}
+          tableProps={tableProps}
+          filterItemRemoved={() => {
+            const filterRemovedArr = filters.filter(
+              (_filter, _idx) => idx !== _idx
+            );
+            setFilters([...filterRemovedArr]);
+          }}
+          filterItemChanged={(filter) => {
+            const updatedFiltersArr = filters.map((_filter, _idx) => {
+              if (_idx === idx) return filter;
+              return _filter;
+            });
+            setFilters([...updatedFiltersArr]);
+          }}
+        />
+      ))}
+    </div>
+  );
+}
+
+type FilterControlsProps = {
+  addFilter: () => void;
+  applyFilters: () => void;
+};
+
+function FilterControls({ addFilter, applyFilters }: FilterControlsProps) {
+  return (
+    <div className="border-t-[1px] border-surface-300 pt-3 flex justify-between">
+      <NButton
+        kind="ghost"
+        className="px-2 py-2"
+        size="xs"
+        title="Add Filter"
+        onClick={(e) => {
+          e.preventDefault();
+          addFilter();
+        }}
+        icon={<AddIcon size={16} />}
+      />
+      <NButton
+        kind="secondary"
+        className="px-2 py-2"
+        title="Apply filter"
+        size="xs"
+        onClick={applyFilters}
+      />
+    </div>
+  );
+}
 
 type NovellaDataTableFixedFilterMenuProps = {
   tableProps: any;
@@ -17,140 +142,45 @@ type NovellaDataTableFixedFilterMenuProps = {
 export default function NDataTableFixedFilterMenu({
   tableProps,
   filterRulesChanged,
-  position = "right",
 }: NovellaDataTableFixedFilterMenuProps) {
   const [filters, setFilters] = useState<NDataTableFixedFilter[]>([]);
+  const defaultFilter = useMemo<NDataTableFixedFilter>(
+    () => ({
+      id: filters.length,
+      operator: "eq",
+      prop: "id",
+      value: "",
+    }),
+    [filters]
+  );
+
+  const addFilter = () => {
+    setFilters([...filters, defaultFilter]);
+  };
+
+  const applyFilters = () => filterRulesChanged(filters);
 
   return (
-    <Popover as="div" className="relative">
-      {({ open }) => (
+    <NDataTableFixedMenu
+      buttonContent={
         <>
-          <Popover.Button className="py-2 px-3 inline-flex text-sm gap-2 justify-center items-center text-surface-700 bg-surface-200 hover:bg-surface-300 transition-all border-[1px] border-surface-200 focus:border-surface-900 disabled:bg-surface-200 disabled:opacity-60 outline-none">
-            <FilterIcon size={18} />
-            {filters.length ? `Filtered by ${filters.length} rule` : "Filter"}
-          </Popover.Button>
-          {open && (
-            <Popover.Panel
-              static
-              className={`absolute ${position}-0 origin-top-${position} mt-1 bg-surface-200 border-[1px] border-surface-300  min-w-[320px] p-3 flex gap-2 flex-col text-sm z-10`}
-            >
-              <div className="flex flex-col gap-2">
-                {!filters.length ? (
-                  <>
-                    <span className="text-surface-700">
-                      No filter applied to this table
-                    </span>
-                    <span className="text-surface-600 text-xs">
-                      Add a column below to filter the view
-                    </span>
-                  </>
-                ) : (
-                  <div className="flex gap-3 items-center w-full text-surface-600">
-                    {filters.map((filter, idx) => (
-                      <div key={filter.id} className="w-full flex-grow flex">
-                        <Select
-                          onChange={(e) => {
-                            const n = filters.map((v, i) => {
-                              if (i === idx)
-                                return {
-                                  id: v.id,
-                                  prop: e.target.value,
-                                  operator: v.operator,
-                                  value: v.value,
-                                };
-                              return v;
-                            });
-                            setFilters(n);
-                          }}
-                          value={filter.prop}
-                          className="flex-grow"
-                        >
-                          {tableProps.map((prop: any) => (
-                            <option key={prop}>{prop}</option>
-                          ))}
-                        </Select>
-                        <Select
-                          onChange={(e) => {
-                            const n = filters.map((v, i) => {
-                              if (i === idx)
-                                return {
-                                  id: v.id,
-                                  prop: v.prop,
-                                  operator: e.target.value,
-                                  value: v.value,
-                                };
-                              return v;
-                            });
-                            setFilters(n);
-                          }}
-                          value={filter.operator}
-                        >
-                          {["eq", "gt", "lt"].map((v) => (
-                            <option key={v}>{v}</option>
-                          ))}
-                        </Select>
-                        <input
-                          value={filter.value}
-                          onChange={(e) => {
-                            e.preventDefault();
-                            setFilters(
-                              filters.map((v, i) => {
-                                if (i == idx)
-                                  return {
-                                    id: v.id,
-                                    prop: v.prop,
-                                    operator: v.operator,
-                                    value: e.target.value,
-                                  };
-                                return v;
-                              })
-                            );
-                          }}
-                          className="py-1 px-2 block min-w-[5rem] bg-surface-200 appearance-none outline-none hover:bg-surface-300 border-[1px] border-surface-200 focus:border-surface-900 text-xs text-surface-900 placeholder:text-surface-500"
-                          placeholder="Enter your value here"
-                        ></input>
-                        <ButtonGhost
-                          icon={<CloseIcon size={12} />}
-                          onClick={() => {
-                            const newFilters = filters.filter((f, i) => {
-                              if (i !== idx) return f;
-                            });
-                            setFilters(newFilters);
-                          }}
-                        />
-                      </div>
-                    ))}
-                  </div>
-                )}
-                <div className="border-t-[1px] border-surface-300 pt-3 flex justify-between">
-                  <ButtonGhost
-                    fontSize="xs"
-                    title="Add Filter"
-                    onClick={(e) => {
-                      e.preventDefault();
-                      setFilters([
-                        ...filters,
-                        {
-                          id: filters.length,
-                          operator: "eq",
-                          prop: "id",
-                          value: "",
-                        },
-                      ]);
-                    }}
-                    icon={<AddIcon size={16} />}
-                  />
-                  <ButtonSecondary
-                    title="Apply filter"
-                    fontSize="xs"
-                    onClick={() => filterRulesChanged(filters)}
-                  />
-                </div>
-              </div>
-            </Popover.Panel>
-          )}
+          <FilterIcon size={18} />
+          {filters.length ? `Filtered by ${filters.length} rule` : "Filter"}
         </>
-      )}
-    </Popover>
+      }
+    >
+      <div className="flex flex-col gap-2">
+        {!filters.length ? (
+          <DisplayEmptyFilters />
+        ) : (
+          <FilterGroup
+            filters={filters}
+            setFilters={setFilters}
+            tableProps={tableProps}
+          />
+        )}
+        <FilterControls addFilter={addFilter} applyFilters={applyFilters} />
+      </div>
+    </NDataTableFixedMenu>
   );
 }
--- src/components/NDataTableFixed/NDataTableFixedSortMenu.tsx
+++ src/components/NDataTableFixed/NDataTableFixedSortMenu.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Popover } from "@headlessui/react";
+import { Dispatch, SetStateAction, useState, Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
 import ButtonGhost from "../ButtonGhost";
 import ButtonSecondary from "../ButtonSecondary";
 import CloseIcon from "../icons/CloseIcon";
@@ -7,6 +7,7 @@ import SortIcon from "../icons/SortIcon";
 import Select from "../Select";
 import NovellaSwitch from "../NovellaSwitch";
 import { NDataTableFixedSort } from ".";
+import NDataTableFixedMenu from "./NDataTableFixedMenu";
 
 type NovellaDataTableFixedSortMenuProps<T> = {
   fields: Array<{ id: keyof T; header: string }>;
@@ -14,91 +15,126 @@ type NovellaDataTableFixedSortMenuProps<T> = {
   position?: "left" | "right";
 };
 
+function DisplayEmptySorts() {
+  return (
+    <>
+      <span className="text-surface-700">
+        No sorting rules applied to this table
+      </span>
+      <span className="text-surface-600 text-xs">
+        Add a column below to sort the view
+      </span>
+    </>
+  );
+}
+
+type DisplayEmptySortsProps<T> = {
+  sorts: NDataTableFixedSort<T> | null;
+  setSorts: Dispatch<SetStateAction<NDataTableFixedSort<T> | null>>;
+};
+function DisplaySortingRules<T>({
+  sorts,
+  setSorts,
+}: DisplayEmptySortsProps<T>) {
+  return (
+    <div className="flex flex-col gap-2 items-center w-full text-surface-600">
+      <div className="flex flex-grow items-center w-full justify-between text-xs">
+        <span>
+          Sorted by<em>{String(sorts!.field)}</em>
+        </span>
+        <div className="flex gap-3 items-center">
+          <div className="flex gap-2 items-center">
+            <label>ascending: </label>
+            <NovellaSwitch
+              defaultValue={sorts!.ascending}
+              onChange={(enabled) => {
+                setSorts({
+                  field: sorts!.field,
+                  ascending: enabled,
+                });
+              }}
+            />
+          </div>
+          <ButtonGhost
+            icon={<CloseIcon size={12} />}
+            onClick={() => {
+              setSorts(null);
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+type SortControlProps<T> = {
+  fields: Array<{ id: keyof T; header: string }>;
+  sorts: NDataTableFixedSort<T> | null;
+  setSorts: Dispatch<SetStateAction<NDataTableFixedSort<T> | null>>;
+  sortRulesChange: (rule: NDataTableFixedSort<T> | null) => void;
+};
+function SortControls<T>({
+  sorts,
+  setSorts,
+  fields,
+  sortRulesChange,
+}: SortControlProps<T>) {
+  return (
+    <div className="border-t-[1px] border-surface-300 pt-3 flex justify-between">
+      <Select
+        onChange={(e) => {
+          setSorts({
+            field: e.target.value as keyof T,
+            ascending: true,
+          });
+        }}
+      >
+        <option disabled selected>
+          Pick a column to sort by
+        </option>
+        {fields.map((field) => (
+          <option>{String(field.id)}</option>
+        ))}
+      </Select>
+      <ButtonSecondary
+        title="Apply sorting"
+        fontSize="xs"
+        onClick={() => sortRulesChange(sorts)}
+      />
+    </div>
+  );
+}
+
 export default function NDataTableFixedSortMenu<T>({
   fields,
   sortRulesChange,
-  position = "right",
 }: NovellaDataTableFixedSortMenuProps<T>) {
   const [sorts, setSorts] = useState<NDataTableFixedSort<T> | null>(null);
+
   return (
-    <Popover as="div" className="relative">
-      {({ open, close }) => (
+    <NDataTableFixedMenu
+      buttonContent={
         <>
-          <Popover.Button className="py-2 px-3 inline-flex text-sm gap-2 justify-center items-center text-surface-700 bg-surface-200 hover:bg-surface-300 transition-all border-[1px] border-surface-200 focus:border-surface-900 disabled:bg-surface-200 disabled:opacity-60 outline-none">
-            <SortIcon size={18} />
-            {sorts ? `Sorted by ${String(sorts.field)}` : "Sort"}
-          </Popover.Button>
-          {open && (
-            <Popover.Panel
-              static
-              className={`absolute ${position}-0 origin-top-${position} mt-1 bg-surface-200 border-[1px] border-surface-300  min-w-[320px] p-3 flex gap-2 flex-col text-sm z-10`}
-            >
-              <div className="flex flex-col gap-2">
-                {!sorts ? (
-                  <>
-                    <span className="text-surface-700">
-                      No sorting rules applied to this table
-                    </span>
-                    <span className="text-surface-600 text-xs">
-                      Add a column below to sort the view
-                    </span>
-                  </>
-                ) : (
-                  <div className="flex flex-col gap-2 items-center w-full text-surface-600">
-                    <div className="flex flex-grow items-center w-full justify-between text-xs">
-                      <span>
-                        Sorted by<em>{String(sorts.field)}</em>
-                      </span>
-                      <div className="flex gap-3 items-center">
-                        <div className="flex gap-2 items-center">
-                          <label>ascending: </label>
-                          <NovellaSwitch
-                            defaultValue={sorts.ascending}
-                            onChange={(enabled) => {
-                              setSorts({
-                                field: sorts.field,
-                                ascending: enabled,
-                              });
-                            }}
-                          />
-                        </div>
-                        <ButtonGhost
-                          icon={<CloseIcon size={12} />}
-                          onClick={() => {
-                            setSorts(null);
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                )}
-                <div className="border-t-[1px] border-surface-300 pt-3 flex justify-between">
-                  <Select
-                    onChange={(e) => {
-                      setSorts({
-                        field: e.target.value as keyof T,
-                        ascending: true,
-                      });
-                    }}
-                  >
-                    <option disabled selected>
-                      Pick a column to sort by
-                    </option>
-                    {fields.map((field) => (
-                      <option>{String(field.id)}</option>
-                    ))}
-                  </Select>
-                  <ButtonSecondary
-                    title="Apply sorting"
-                    fontSize="xs"
-                    onClick={() => sortRulesChange(sorts)}
-                  />
-                </div>
-              </div>
-            </Popover.Panel>
-          )}
+          <SortIcon size={18} />
+          {sorts ? `Sorted by ${String(sorts.field)}` : "Sort"}
         </>
-      )}
-    </Popover>
+      }
+    >
+      <>
+        <div className="flex flex-col gap-2">
+          {!sorts ? (
+            <DisplayEmptySorts />
+          ) : (
+            <DisplaySortingRules sorts={sorts} setSorts={setSorts} />
+          )}
+        </div>
+        <SortControls
+          sorts={sorts}
+          setSorts={setSorts}
+          fields={fields}
+          sortRulesChange={sortRulesChange}
+        />
+      </>
+    </NDataTableFixedMenu>
   );
 }
