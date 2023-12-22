d4ce1ace6c60b184697425c7202d762aa1e42d5a
Iyaan Azeez <iyaanazeez757@gmail.com>
2023-12-22 19:39:33 +0500

    Fixed: Internal Server Error in production when trying to access /dashboard The middleware function was not being called properly. Also the supabase getUser functions was throwing an error which was not handled properly.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,4 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+import { User, createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 
 import type { NextRequest } from "next/server";
@@ -6,10 +6,24 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
   const supabase = createMiddlewareClient({ req, res });
+  let user: User | null = null;
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  try {
+    const { data } = await supabase.auth.getUser();
+    if (!data.user) {
+      throw new Error("Auth Error: Could not access user session");
+    }
+    user = data.user;
+  } catch (error) {
+    const errorFormatted = new Error(
+      "Auth Error: Error occcured when trying to access session",
+      {
+        cause: error,
+      }
+    );
+    console.error(errorFormatted);
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
 
   // if user is signed in and the current path is / redirect the user to /dashboard
   if (user && req.nextUrl.pathname === "/") {
@@ -27,7 +41,7 @@ export async function middleware(req: NextRequest) {
 export const config = {
   matcher: [
     "/",
-    "/dasboard/:path*",
+    "/dashboard",
     "/books/:path*",
     "/students/:path*",
     "/issued/:path*",
